17dda6f2770c9f4ecad495491e17f915141fb960
chemicalspaceman <36962002+chemicalspaceman@users.noreply.github.com>
2019-01-28 01:30:11 +0000

    Add files via upload

---
+++ levels.js
@@ -0,0 +1,25 @@
+var level1 = [
+ 	["black","black","black","black","black"],
+ 	["black","player","green","gold","black"],
+ 	["black","black","blue","black","black"],
+ 	["black","black","black","black","black"]
+];
+
+
+var level2 = [
+ 	["black","black","black","black","black","black"],
+ 	["black","player","green","red","gold","black"],
+ 	["black","black","green","blue","black","black"],
+ 	["black","black","black","black","black","black"]
+];
+
+var level3 = [
+ 	["black","black","black","black","black"],
+ 	["black","player","green","gold","black"],
+ 	["black","black","blue","black","black"],
+ 	["black","black","black","black","black"]
+];
+
+
+
+
